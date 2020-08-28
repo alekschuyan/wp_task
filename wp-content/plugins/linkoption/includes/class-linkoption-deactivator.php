0856191daff707c1c03951ec11f758e49687ec85
Aleksander Chuyan <aleksander.chuyan@gmail.com>
2020-08-28 22:26:40 +0300

    add: plugin create

---
+++ wp-content/plugins/linkoption/includes/class-linkoption-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       -
+ * @since      1.0.0
+ *
+ * @package    Linkoption
+ * @subpackage Linkoption/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Linkoption
+ * @subpackage Linkoption/includes
+ * @author     Aleks Ch <aleksander.chuyan@gmail.com>
+ */
+class Linkoption_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
